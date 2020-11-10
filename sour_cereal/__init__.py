2648f1013baca4cab3c7ce7cdc8b1f5b23a22427
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2020-11-10 09:52:27 -0300

    Provide essencial classes on the top level of the project

---
+++ sour_cereal/__init__.py
@@ -0,0 +1,4 @@
+from .base.extraction import Extraction     # noqa
+from .source_connection import SourceConnection     # noqa
+from .auth_source_connection import AuthSourceConnection    # noqa
+from .session_source_connection import SessionSourceConnection  # noqa

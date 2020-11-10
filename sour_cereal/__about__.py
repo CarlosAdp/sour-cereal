aa71eda79c8f8eaab43fb92ee8f3f4a1718c2841
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2020-11-10 08:25:27 -0300

    Make package installable

---
+++ sour_cereal/__about__.py
@@ -0,0 +1,17 @@
+name = 'sour-cereal'
+version = '0.1'
+
+author = 'Carlos Alberto Duarte Pinto'
+author_email = 'carlos.adpinto@gmail.com'
+
+description = 'An abstraction of data source for extraction applications usage'
+
+url = 'https://github.com/CarlosAdp/sour-cereal'
+
+classifiers = [
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+    'Topic :: Utilities'
+]
+
+license = 'Brazilian Army'

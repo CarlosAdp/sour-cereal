0a798dce559d2696e655d8917e7214054ff0a6d4
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2020-11-11 10:58:03 -0300

    Add wikipedia example

---
+++ sour_cereal/examples/wikipedia/main.py
@@ -0,0 +1,47 @@
+'''This example creates a custom connection to Wikipedia used to retrieve its
+pages tables in an easy way.'''
+
+import pandas
+from sour_cereal import SourceConnection
+
+
+class WikiTableConnection(SourceConnection):
+    def execute_extraction(
+        self: 'WikiTableConnection',
+        parameters: dict
+    ) -> pandas.DataFrame:
+        table = pandas.read_html(parameters['url'])[parameters['table_index']]
+        table = table.iloc[1:]
+        return table
+
+
+con = WikiTableConnection()
+
+parameters = {
+    'gdps': {
+        'url': 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nomi' +
+        'nal)',
+        'table_index': 7
+
+    },
+    'companies': {
+        'url': 'https://en.wikipedia.org/wiki/List_of_largest_companies_by_r' +
+        'evenue',
+        'table_index': 0
+    }
+}
+
+extractions = {
+    tag: con.new_extraction(parameters=parameters[tag])
+    for tag in parameters
+}
+
+results = {
+    tag: extractions[tag].execute()
+    for tag in parameters
+}
+
+for tag, result in results.items():
+    print(tag + ':' + '\n---------------------------------------\n')
+    print(result)
+    print('\n')
